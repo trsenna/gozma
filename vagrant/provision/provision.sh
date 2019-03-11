269d977281a9bab64c7bf9aeb791b936e830340d
Thiago Senna <trsenna@gmail.com>
2019-03-10 23:44:00 -0300

    working one file only

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -57,17 +57,74 @@ service apache2 restart
 
 
 #====================================================
+#== Database
+#====================================================
+echo "mysql-server mysql-server/root_password password root@secret" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root@secret" | debconf-set-selections
+
+apt-get -y install \
+  mysql-server \
+  postgresql postgresql-contrib \
+  sqlite3 libsqlite3-dev
+
+#== service restart ==
+service mysql restart
+service postgresql restart
+
+
+
+
+
+#====================================================
 #== Extras
 #====================================================
 
 #== Node.js ==
-# if [ ! -f "/usr/bin/node" ]; then
-#   curl -sL https://deb.nodesource.com/setup_8.x | bash -
-#   apt-get install -y nodejs
-# fi
+if [ ! -f "/usr/bin/node" ]; then
+  curl -sL https://deb.nodesource.com/setup_8.x | bash -
+  apt-get install -y nodejs
+fi
 
+#== Java SDK ==
+apt-get -y install \
+  openjdk-8-jdk \
+  openjdk-8-source
 
+#== SDKMAN & SDKs ==
+if [ ! -d "/home/vagrant/.sdkman" ]; then
+  su - vagrant -c 'curl -s "https://get.sdkman.io" | bash'
+  su - vagrant -c 'source /home/vagrant/.sdkman/bin/sdkman-init.sh && yes | sdk install groovy'
+  su - vagrant -c 'source /home/vagrant/.sdkman/bin/sdkman-init.sh && yes | sdk install gradle'
+  su - vagrant -c 'source /home/vagrant/.sdkman/bin/sdkman-init.sh && yes | sdk install maven'
+fi
 
+#== Composer ==
+if [ ! -f "/usr/local/bin/composer" ]; then
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+fi
+
+#== WP-CLI ==
+if [ ! -f "/usr/local/bin/wp" ]; then
+  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
+  mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ ! -f "/usr/local/bin/mailhog" ]; then
+  wget --quiet -O /usr/local/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64
+  chmod +x /usr/local/bin/mailhog
+fi
+
+if [ ! -f "/usr/local/bin/mhsendmail" ]; then
+  wget --quiet -O /usr/local/bin/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
+  chmod +x /usr/local/bin/mhsendmail
+fi
+
+if [ ! -d "/home/vagrant/.oh-my-zsh" ]; then
+  su - vagrant -c 'git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh'
+  su - vagrant -c 'cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc'
+fi
 
 #====================================================
 #== Cleanup
